8d86ab9f6cbdf1b97d5e54b438387a01be176b4c
Michael Khaikin <michael.khaikin@gmail.com>
2018-05-13 20:03:47 -0400

    updated output line

--- calc.js
+++ calc.js
@@ -1,30 +1,27 @@
 $(document).ready(function() {
-  //  function clearDisplay(){
-  //   $(function(){
-  //     $('#display').val("5");
-  //   });
-  // }
+	//  function clearDisplay(){
+	//   $(function(){
+	//     $('#display').val("5");
+	//   });
+	// }
 
-  $('#clearButton').on("click",function(){
-    firstValue.splice(0,firstValue.length);
-  });
-  // var result = 0;
-  // $('button').click(function() {
-  //   let val = $(this).val();
-  //   result =$('#display').val(val);
-  // });
-  let firstValue= [];
-  $(function(){
-    $('button').on("click", function(){
-      let val = $(this).val();
-      firstValue.push(Number(val));
-      console.log(firstValue);
-     $('#display').val(
-       for(let i=0;i<firstValue.length;i++){
-         num[i];
-       }
-     );
-    });
-  });
-
+	$('#clearButton').on('click', function() {
+		firstValue.splice(0, firstValue.length);
+	});
+	// var result = 0;
+	// $('button').click(function() {
+	//   let val = $(this).val();
+	//   result =$('#display').val(val);
+	// });
+	let firstValue = [];
+	$(function() {
+		$('button').on('click', function() {
+			let val = $(this).val();
+			firstValue.push(Number(val));
+			console.log(firstValue);
+			for (let i = 0; i < firstValue.length; i++) {
+				$('#output').val(firstValue[i]);
+			}
+		});
+	});
 });
