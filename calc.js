8794aa10cdb5da3f1d12943ac1a74bdc40a7693b
Michael Khaikin <michael.khaikin@gmail.com>
2018-05-14 22:50:12 -0400

    factorized

--- calc.js
+++ calc.js
@@ -1,31 +1,3 @@
-<<<<<<< HEAD
-$(document).ready(function() {
-	//  function clearDisplay(){
-	//   $(function(){
-	//     $('#display').val("5");
-	//   });
-	// }
-
-	$('#clearButton').on('click', function() {
-		firstValue.splice(0, firstValue.length);
-	});
-	// var result = 0;
-	// $('button').click(function() {
-	//   let val = $(this).val();
-	//   result =$('#display').val(val);
-	// });
-	let firstValue = [];
-	$(function() {
-		$('button').on('click', function() {
-			let val = $(this).val();
-			firstValue.push(Number(val));
-			console.log(firstValue);
-			for (let i = 0; i < firstValue.length; i++) {
-				$('#output').val(firstValue[i]);
-			}
-		});
-	});
-=======
 //   let firstValue = [];
 //   let $output = '';
 //
