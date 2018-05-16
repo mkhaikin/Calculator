5dbf78b4e36845b18cd6864371f456da7d4add43
Michael Khaikin <michael.khaikin@gmail.com>
2018-05-16 00:33:42 -0400

    updated display function

--- calc.js
+++ calc.js
@@ -1,62 +1,30 @@
-//   let firstValue = [];
-//   let $output = '';
-//
-// $(document).ready(function() {
-//   $('#clearButton').on("click",function(){
-//     while(firstValue.length > 0){
-//       firstValue.pop();
-//     }
-//   });
-//
-//   $('#equalsButton').on("click",function(){
-//     alert($output);
-//   });
-//
-//   $('#addButton').on("click",function(){
-//     alert($output);
-//   });
 
+	 $("button").on("click", function(){
+	    if($(this).attr("value")){
+        $('#output').text($('#output').html()+$(this).text());
+        $('#display').val($('#display').val()+$(this).text());
+        console.log($(this).text());
+      } else{
+  	     if($('#output').text()!=''){
+           if(this.id == 'equalsButton'){
+             $('#output').html($('#output').html());
+             console.log($(this).text());
+           } else {
+             $('#output').html($('#output').html()+$(this).text());
+             console.log($(this).val());
+           }
+         }
+       }
+	 });
 
-   //  $('button').on("click", function(){
-   //   let value = $(this).val();
-   //   firstValue.push(Number(value));
-   //   $('#output').html('');
-   //   if($(this).attr('value')){
-   //     for(let i = 0; i < firstValue.length; i++) {
-   //       if(firstValue[0]==0){
-   //         firstValue.pop();
-   //       } else{
-   //        $('#output').append(firstValue[i]);
-   //        $output = $('#output').html();
-   //       }
-   //     }
-   //     // console.log($output);
-   //   } else{
-   //
-   //   }
-   // });
+	 $("#clearButton").on("click",function(){
+	   $('#output').html('');
+     $('#display').val('');
+	 });
 
-
-
-// });
-var operators = [$(addButton),$(subtractButton),$(multiplyButton),$(divideButton)];
-
-$("button").on("click", function(){
-  if($(this).attr('data-role')!='operator'){
-    $('#output').html($('#output').html()+$(this).val());
-  } else{
-    if($('#output').val()!=''){
-      $('#output').html($('#output').html()+$(this).val());
-    }
-  }
-});
-
-$("#clearButton").on("click",function(){
-  $('#output').html('');
-});
-
-$("#equalsButton").on("click", function(){
-  var expression = $('#output').html();
-  $('#output').html(eval(expression));
-});
-
+	 $("#equalsButton").on("click", function(){
+	    var expression = $('#output').text();
+	    var total = (eval(expression));
+      $('#display').val(total);
+	 	console.log(total);
+	 });
