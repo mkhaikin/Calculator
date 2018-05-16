d0c17eb43e714653034aac76f32ab5a93ccdab3a
Michael Khaikin <michael.khaikin@gmail.com>
2018-05-16 01:19:28 -0400

    updated divide funct

--- calc.js
+++ calc.js
@@ -1,30 +1,34 @@
 
-	 $("button").on("click", function(){
-	    if($(this).attr("value")){
-        $('#output').text($('#output').html()+$(this).text());
-        $('#display').val($('#display').val()+$(this).text());
-        console.log($(this).text());
-      } else{
-  	     if($('#output').text()!=''){
-           if(this.id == 'equalsButton'){
-             $('#output').html($('#output').html());
-             console.log($(this).text());
-           } else {
+$("button").on("click", function(){
+    if($(this).attr("value")){
+      $('#output').text($('#output').html()+$(this).text());
+      $('#display').val($('#display').val()+$(this).text());
+      console.log($(this).text());
+    } else{
+	     if($('#output').text()!=''){
+         if(this.id == 'equalsButton'){
+           $('#output').html($('#output').html());
+           console.log($(this).text());
+         } else if(this.id == 'divideButton'){
+           $('#display').val('');
+           $('#output').html($('#output').html()+ "/")
+           }else {
              $('#output').html($('#output').html()+$(this).text());
+             $('#display').val('');
              console.log($(this).val());
            }
-         }
        }
-	 });
+     }
+});
 
-	 $("#clearButton").on("click",function(){
-	   $('#output').html('');
-     $('#display').val('');
-	 });
+$("#clearButton").on("click",function(){
+	$('#output').html('');
+	$('#display').val('');
+});
 
-	 $("#equalsButton").on("click", function(){
-	    var expression = $('#output').text();
-	    var total = (eval(expression));
-      $('#display').val(total);
-	 	console.log(total);
-	 });
+$("#equalsButton").on("click", function(){
+	var expression = $('#output').text();
+	var total = (eval(expression));
+	$('#display').val(total);
+	console.log(total);
+});
